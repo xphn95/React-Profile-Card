45d45ecb514c4cfd8351c0c7089cd10cc3f162f4
小屁孩 <66484849+xphn95@users.noreply.github.com>
2024-12-11 09:50:52 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,73 @@
-import "./styles.css"
+import './styles.css'
+
+const skills = [
+  {
+    skill: 'HTML+CSS',
+    level: 'advanced',
+    color: '#2662EA'
+  },
+  {
+    skill: 'JavaScript',
+    level: 'advanced',
+    color: '#EFD81D'
+  },
+  {
+    skill: 'Web Design',
+    level: 'advanced',
+    color: '#C3DCAF'
+  },
+  {
+    skill: 'Git and GitHub',
+    level: 'intermediate',
+    color: '#E84F33'
+  },
+  {
+    skill: 'React',
+    level: 'advanced',
+    color: '#60DAFB'
+  },
+  {
+    skill: 'Svelte',
+    level: 'beginner',
+    color: '#FF3B00'
+  }
+]
+
+const Avatar = () => <img className="avatar" src="jonas.jpeg" />
+
+const Intro = () => (
+  <div>
+    <h1>Jonas Schmedtmann</h1>
+    <p>
+      Full-stack web developer and teacher at Udemy. When not coding or
+      preparing a course, I like to play board games, to cook (and eat), or to
+      just enjoy the Portuguese sun at the beach.
+    </p>
+  </div>
+)
+
+const Skill = ({ skill, color }) => (
+  <div className="skill" style={{ backgroundColor: color }}>
+    <span>{skill}</span>
+  </div>
+)
+
+const SkillList = () => (
+  <div className="skill-list">
+    {skills.map(item => (
+      <Skill skill={item.skill} bgColor={item.color} />
+    ))}
+  </div>
+)
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
     </div>
   )
 }
